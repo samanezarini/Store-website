36d7da51baa2ce97e4e0b483928a808052c5de1e
samane zarini <samane_zarini@yahoo.com>
2024-11-25 17:21:06 +0330

    add USD hover

--- src/components/Home/Nav.js
+++ src/components/Home/Nav.js
@@ -22,20 +22,20 @@ function Nav(){
                                 </div>
                             </Col>
                             <Col lg={6} md={5} className="d-flex justify-content-end h-25">
-                                <div className="d-flex gap-5 mt-2 text-light" style={{ fontSize: "0.85rem" }}>
+                                <div className="head d-flex gap-5 mt-2 text-light" style={{ fontSize: "0.86rem" }}>
                                     <div className="d-flex gap-5">
-                                        <span><a>SIGN IN</a></span>
-                                        <span><a>FAQS</a></span>
+                                        <span>SIGN IN</span>
+                                        <span>FAQS</span>
                                     </div>
                                     <div className="usd-hover">
-                                        <span className="text-light d-flex">
-                                            <a>USD</a>
+                                        <span className="text-light d-flex dropdown-toggle">
+                                            <a href="">USD</a>
                                             <IoIosArrowDown className="w-75 h-50 mt-1 ms-1" />
                                         </span>
-                                        <ul>
-                                            <li>USD</li>
-                                            <li>EUR</li>
-                                            <li>USD</li>
+                                        <ul className="dropdown-menu">
+                                            <a href=""><li>USD</li></a>
+                                            <a href=""><li>EUR</li></a>
+                                            <a href=""><li>USD</li></a>
                                         </ul>
                                     </div>
                                 </div>
